c72e1856e595262e44d9a34c7dc3d1b3478111cc
qustandifashho <kostyfashho@gmail.com>
2024-05-02 10:32:55 -0500

    rank

--- app/src/main/java/com/example/filmapptest/RankedListActivity.java
+++ app/src/main/java/com/example/filmapptest/RankedListActivity.java
@@ -24,15 +24,13 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
-public class RankedListActivity extends ComponentActivity
-{
+public class RankedListActivity extends ComponentActivity {
     ArrayList<Movie> movies;
     RecyclerView movieList;
     Adapter adapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ranklist);
 
@@ -46,17 +44,15 @@ public class RankedListActivity extends ComponentActivity
         Movie three = new Movie("Best movie of all time", "Todd Howard", "Action", 1.0f);
 
 
-
         //Setup the view
         movieList = findViewById(R.id.rankList);
         movieList.setLayoutManager(new LinearLayoutManager(this));
         adapter = new Adapter(movies, this);
         movieList.setAdapter(adapter);
 
-        refresh.setOnClickListener(new View.OnClickListener(){
+        refresh.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view0)
-            {
+            public void onClick(View view0) {
                 //Update the movies arraylist
 
                 //Sort the movies using the compare function below
@@ -68,18 +64,15 @@ public class RankedListActivity extends ComponentActivity
         });
     }
 
-    public int getID()
-    {
+    public int getID() {
         Intent intent = getIntent();
         int id = intent.getIntExtra("id", -1);
         return id;
     }
 
-    private class MovieComparator implements Comparator<Movie>
-    {
+    private class MovieComparator implements Comparator<Movie> {
         @Override
-        public int compare(Movie m1, Movie m2)
-        {
+        public int compare(Movie m1, Movie m2) {
             Integer m1Rating = (int) (m1.getRating() * 100);
             Integer m2Rating = (int) (m2.getRating() * 100);
 
@@ -87,58 +80,52 @@ public class RankedListActivity extends ComponentActivity
             int nameTieBreaker = m1.getTitle().compareTo(m2.getTitle());
             int genreTieBreaker = m1.getGenre().compareTo(m2.getGenre());
 
-            if(ratingTieBreaker != 0) return ratingTieBreaker;
-            else if(nameTieBreaker != 0) return nameTieBreaker;
+            if (ratingTieBreaker != 0) return ratingTieBreaker;
+            else if (nameTieBreaker != 0) return nameTieBreaker;
             else return genreTieBreaker;
         }
     }
 
-    private class Adapter extends RecyclerView.Adapter<ViewHolder>
-    {
+    private class Adapter extends RecyclerView.Adapter<ViewHolder> {
         ArrayList<Movie> movies;
         RankedListActivity rlActivity;
 
-        public Adapter(ArrayList<Movie> movies, RankedListActivity rlActivity)
-        {
+        public Adapter(ArrayList<Movie> movies, RankedListActivity rlActivity) {
             this.movies = movies;
             this.rlActivity = rlActivity;
         }
 
         @Override
-        public int getItemCount()
-        {
+        public int getItemCount() {
             return movies.size();
         }
 
         @NonNull
         @Override
-        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
-        {
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
             LayoutInflater inflater = LayoutInflater.from(rlActivity);
             return new ViewHolder(inflater, parent);
         }
 
         @Override
-        public void onBindViewHolder(@NonNull ViewHolder holder, int position)
-        {
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
             Movie movie = movies.get(position);
             holder.bind(movie);
         }
 
-        public ArrayList<Movie> getMovies()
-        {
+        public ArrayList<Movie> getMovies() {
             return movies;
         }
     }
 
-    private class ViewHolder extends RecyclerView.ViewHolder
+    private static class ViewHolder extends RecyclerView.ViewHolder
     {
         TextView information;
 
         public ViewHolder(LayoutInflater inflater, ViewGroup parent)
         {
-            super(inflater.inflate(R.layout.movie_item, parent, false));
-            information = itemView.findViewById(R.id.movie_info);
+            super(inflater.inflate(R.layout.addmovies, parent, false));
+            information = itemView.findViewById(R.id.movieGenre);
         }
 
         public void bind(Movie movie)
