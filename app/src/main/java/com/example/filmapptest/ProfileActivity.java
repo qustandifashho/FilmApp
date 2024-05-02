a8c9afc73789d265010bf8f69d389b5fe4c1d8a8
lucia <lucianit3@gmail.com>
2024-05-02 00:21:55 -0500

    hi commit

--- app/src/main/java/com/example/filmapptest/ProfileActivity.java
+++ app/src/main/java/com/example/filmapptest/ProfileActivity.java
@@ -4,20 +4,26 @@ import androidx.activity.ComponentActivity;
 import android.content.Intent;
 import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class ProfileActivity extends ComponentActivity {
     private Account profileinfo;
-    private AssetManager assets;
+    //private AssetManager assets;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
-        assets = getAssets();
+        //assets = getAssets();
         setupProfile(); //hi
+        setupButtons();
     }
 
     public void setupProfile(){
@@ -27,26 +33,55 @@ public class ProfileActivity extends ComponentActivity {
         Scanner scnr;
         String str ="";
         String[] arr = null;
+        File f = new File(getFilesDir().getAbsolutePath() + "/accounts.txt");
 
         try{
-            scnr = new Scanner(assets.open("accounts.txt"));
-            while(scnr.hasNext()) {
-                str = scnr.nextLine();
-                arr = str.split(",");
-                if(Integer.parseInt(arr[0])==id){
-                    profileinfo = new Account(id,arr[1],arr[2]);
-                    break;
+            if(f.exists()) {
+                scnr = new Scanner(openFileInput("accounts.txt"));
+                while (scnr.hasNext()) {
+                    str = scnr.nextLine();
+                    arr = str.split(",");
+                    if (Integer.parseInt(arr[0]) == id) {
+                        profileinfo = new Account(id, arr[1], arr[2]);
+                        break;
+                    }
                 }
+                scnr.close();
             }
-            scnr.close();
         }
         catch(IOException e){
             System.out.println("Error: "+ e.getMessage());
         }
-        TextView name = (TextView) findViewById(R.id.name);
-        TextView email = (TextView) findViewById(R.id.email);
-        name.setText(profileinfo.getName());
-        email.setText(profileinfo.getEmail());
+        if (profileinfo != null) {
+            TextView name = (TextView) findViewById(R.id.name);
+            TextView email = (TextView) findViewById(R.id.email);
+            name.setText(profileinfo.getName());
+            email.setText(profileinfo.getEmail());
+        }
+    }
+    private void setupButtons(){
+        Button button1 = (Button) findViewById(R.id.friends);
+        Button button2 = (Button) findViewById(R.id.rate);
+        Button button3 = (Button) findViewById(R.id.watch);
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfileActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfileActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfileActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
 
