df55138a8638de2644322671bcab9a3bdf75bf9b
IYAN M <iyanmomin@gmail.com>
2024-05-02 11:09:11 -0500

    add friend button

--- app/src/main/java/com/example/filmapptest/ProfileActivity.java
+++ app/src/main/java/com/example/filmapptest/ProfileActivity.java
@@ -67,7 +67,8 @@ public class ProfileActivity extends ComponentActivity {
         Button button4 = (Button) findViewById(R.id.logout);
         button1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(ProfileActivity.this, RegisterActivity.class);
+                Intent intent = new Intent(ProfileActivity.this, FriendsListActivity.class);
+                intent.putExtra("id", getId());
                 startActivity(intent);
             }
         });
